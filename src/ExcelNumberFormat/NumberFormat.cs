dae2d92a37f0aae65ca942300511dc9d03f57e46
andersnm <andersnm@users.noreply.github.com>
2017-11-01 10:11:30 +0100

    Backported XML comments + minor tweaks from ExcelDataReader

--- src/ExcelNumberFormat/NumberFormat.cs
+++ src/ExcelNumberFormat/NumberFormat.cs
@@ -4,8 +4,15 @@ using System.Globalization;
 
 namespace ExcelNumberFormat
 {
+    /// <summary>
+    /// Parse ECMA-376 number format strings and format values like Excel and other spreadsheet softwares.
+    /// </summary>
     public class NumberFormat
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NumberFormat"/> class.
+        /// </summary>
+        /// <param name="formatString">The number format string.</param>
         public NumberFormat(string formatString)
         {
             var sections = Parser.ParseSections(formatString, out bool syntaxError);
@@ -16,6 +23,8 @@ namespace ExcelNumberFormat
             if (IsValid)
             {
                 Sections = sections;
+                IsDateTimeFormat = Evaluator.GetFirstSection(Sections, SectionType.Date) != null;
+                IsTimeSpanFormat = Evaluator.GetFirstSection(Sections, SectionType.Duration) != null;
             }
             else
             {
@@ -23,28 +32,34 @@ namespace ExcelNumberFormat
             }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the number format string is valid.
+        /// </summary>
         public bool IsValid { get; }
 
+        /// <summary>
+        /// Gets the number format string.
+        /// </summary>
         public string FormatString { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether the format represents a DateTime
+        /// </summary>
+        public bool IsDateTimeFormat { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether the format represents a TimeSpan
+        /// </summary>
+        public bool IsTimeSpanFormat { get; }
 
         internal List<Section> Sections { get; }
 
-        public bool IsDateTimeFormat
-        {
-            get
-            {
-                return Evaluator.GetFirstSection(Sections, SectionType.Date) != null;
-            }
-        }
-
-        public bool IsTimeSpanFormat
-        {
-            get
-            {
-                return Evaluator.GetFirstSection(Sections, SectionType.Duration) != null;
-            }
-        }
-
+        /// <summary>
+        /// Formats a value with this number format in a specified culture.
+        /// </summary>
+        /// <param name="value">The value to format.</param>
+        /// <param name="culture">The culture to use for formatting.</param>
+        /// <returns>The formatted string.</returns>
         public string Format(object value, CultureInfo culture)
         {
             if (!IsValid || string.IsNullOrEmpty(FormatString))
